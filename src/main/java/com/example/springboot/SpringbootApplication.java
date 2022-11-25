47ec98390f8d6f7d5409d87ee4eb5497865d590c
smartobi <agbawochinonso488@gmail.com>
2022-11-25 23:23:21 +0100

    quality gate status

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,7 +2,6 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.springboot.SpringApplicationTests;
 
 @SpringBootApplication
 public class SpringbootApplication {
