c9f6da57f24775dea4d54a536332f1bcd9131dfa
smartobi <agbawochinonso488@gmail.com>
2022-11-25 23:21:02 +0100

    nexus stage updated

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,7 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.springbootcom.SpringApplicationTests
+import com.example.springboot.SpringApplicationTests;
 
 @SpringBootApplication
 public class SpringbootApplication {
